03e6bb1523f432256800875188c3e84d5fe1cc74
Michael <michaelgranados@gmail.com>
2018-07-07 02:47:02 -0300

    action and adjustments
    Signed-off-by: Michael <michaelgranados@gmail.com>

--- app/src/js/main.js
+++ app/src/js/main.js
@@ -4,8 +4,9 @@ import App from './App'
 new Vue({
   el: '#app',
   render: h => h(App),
+  created: function () {
+    console.log('bla')
+  },
   components: { App },
-  template: `
-    <App/>
-  `,
+  template: '<App/>',
 });
